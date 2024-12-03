d9c1be99bdcd29b4251413ae53c5f835c72cddf9
PrainLopez <qym200042@gmail.com>
2024-12-03 16:38:58 +0000

    Day 01 1/1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,41 @@
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.text.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var list1 = mutableListOf<Int>()
+        var list2 = mutableListOf<Int>()
+
+        for (line in input) {
+            val splitted = line.split(("\\s+".toRegex()))
+
+            list1.add(splitted[0].toInt())
+            list2.add(splitted[1].toInt())
+        }
+
+        list1.sort()
+        list2.sort()
+
+        var sum = 0;
+        for (i in 0..input.size - 1) {
+            val distance = abs(list1[i] - list2[i])
+            sum += distance
+        }
+
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): String {
+        return input[0]
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
